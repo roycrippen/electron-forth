668c6159d0a3e33ebdd95afd3f108ada25c8a24a
roy.crippen <roy.crippen@archarithms.com>
2019-06-14 14:41:52 -0500

    include a library only once

--- src/renderer/engine/include.js
+++ src/renderer/engine/include.js
@@ -6,32 +6,45 @@ import fs from 'fs'
 import InputExceptions from './input-exceptions'
 
 function Include (f) {
+    let includes = []
+
+    const isIncluded = (file) => {
+        return includes.indexOf(file) > -1
+    }
+
     f.defjs("include", function () {
         var outputCallback = f.outputCallback;
 
         var file = f._readWord();
-        if (process.browser || file.match(/^http/)) {
-            if (process.browser) file = url.resolve(location.href, file);
-            request.get(file, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    f.run(body, outputCallback, file.toString());
-                } else {
-                    console.error("Failed to load http file " + file + ". " + error);
-                }
-            });
+        if (isIncluded(file)) {
+            console.log(`file: ${file} is already included, nothing done.`)
+            return
         } else {
-            fs.readFile(file, "utf8", function (error, body) {
-                if (!error) {
-                    f.writeMessage('_', `loaded: ${file}`)
-                    f.run(body, outputCallback, file);
-                } else {
-                    const err = `Failed to load file ${file}. ${error}`
-                    console.error(err)
-                    f.writeMessage('_', err)
-                }
-            });
+            if (process.browser || file.match(/^http/)) {
+                if (process.browser) file = url.resolve(location.href, file);
+                request.get(file, function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        includes.push(file)
+                        f.run(body, outputCallback, file.toString());
+                    } else {
+                        console.error("Failed to load http file " + file + ". " + error);
+                    }
+                });
+            } else {
+                fs.readFile(file, "utf8", function (error, body) {
+                    if (!error) {
+                        f.writeMessage('_', `loaded: ${file}`)
+                        includes.push(file)
+                        f.run(body, outputCallback, file);
+                    } else {
+                        const err = `Failed to load file ${file}. ${error}`
+                        console.error(err)
+                        f.writeMessage('_', err)
+                    }
+                });
+            }
+            throw InputExceptions.WaitingOnInput;
         }
-        throw InputExceptions.WaitingOnInput;
     });
 
     return f;
