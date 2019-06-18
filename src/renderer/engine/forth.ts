bd601cf095ab2187c7f58343d00b5f391c17897b
nathan-smeal <nathan.i.smeal@gmail.com>
2019-06-18 14:37:30 -0500

    Moved data.ts into Forth as proper properties

--- src/renderer/engine/forth.ts
+++ src/renderer/engine/forth.ts
@@ -2,7 +2,6 @@
 
 import NumericOperations from './numeric-operations';
 import Output from './output';
-import Data from './data'
 import Definitions from './definitions'
 import BooleanOperations from './boolean-operations'
 import StackOperations from './stack-operations'
@@ -12,12 +11,19 @@ import JsInterop from './js-interop'
 import Input from './input'
 import Include from './include'
 import Interpreter from './interpreter'
+import Stack from './stack';
 
 class Forth {
+    public instructionPointer: number = 0;
+    public dataSpace: [] = [];
+    public returnStack: Stack = new Stack("Return Stack");
+    public stack: Stack = new Stack("Stack");
+    
     public constructor() {
-        let forth = {};
+        // this will let us use end around dictionary and 
+        // have properties persist because returning 
+        let forth = Object.assign({}, this);
 
-        new Data(forth);
         new Definitions(forth);
         Input(forth);
         new NumericOperations(forth);
